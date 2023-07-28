44379fef05c428661b9fd4b5384c344c2d1b00da
Kyung Kwon <kyungkwon01@gmail.com>
2023-07-27 20:24:09 -0600

    merge conflict resolved

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,7 +1,7 @@
-// 
+//
 const seedUsers = require('./user-seeds');
 const seedTasks = require('./task-seeds');
-const seedProjects = require('./project-seeds')
+const seedProjects = require('./project-seeds');
 
 //
 const sequelize = require('../config/connection');
@@ -18,11 +18,10 @@ const seedAll = async () => {
   console.log('\n----- TASKS SEEDED -----\n');
 
   await seedProjects();
-  console.log(`\n----- PROJECTS SEEDED -----\n`)
-
+  console.log('\n----- PROJECTS SEEDED -----\n');
 
   process.exit(0);
-}
+};
 
 // All databases are seeded. //
 seedAll();
