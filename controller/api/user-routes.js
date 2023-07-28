41b9095fefd770ea7671174c4033c8e516c932fe
Maridon Romney <mkromney@gmail.com>
2023-07-27 21:08:28 -0600

    added routes code for projects and users.

---
+++ controller/api/user-routes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+// const withAuth = require('../../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    const userData = await User.finAll();
+    const users = userData.map((user) => user.toJSON());
+    res.status(200).render('/', { users: users });
+    console.log(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
