65eeabce9c73fcb5ccffe4e390ec818d4e9e827a
Chad Bowler <127648744+ChadBowler@users.noreply.github.com>
2023-07-27 21:11:40 -0600

    Update project-routes.js
    fixed a typo

--- controller/api/project-routes.js
+++ controller/api/project-routes.js
@@ -4,7 +4,7 @@ const withAuth = require('../../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    const projectData = await Projects.finAll();
+    const projectData = await Projects.findAll();
     const projects = projectData.map((project) => project.toJSON());
     res.status(200).render('/', { projects });
     console.log(project);
