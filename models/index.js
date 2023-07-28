6a4d67d5dbf01c1f453b6dcc4581f6c045c312a4
Maridon Romney <mkromney@gmail.com>
2023-07-27 20:24:14 -0600

    added associations. needs review.

---
+++ models/index.js
@@ -0,0 +1,37 @@
+const User = require('./User');
+const Task = require('./Task');
+const Project = require('./Project');
+
+
+Project.hasmany(User, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Project.hasmany(Task, {
+  foreignKey: 'task_id',
+  onDelete: 'CASCADE'
+});
+
+User.hasmany(Task, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+User.belongsTo(Project, {
+  foreignKey: 'project_id',
+  onDelete: 'CASCADE'
+});
+
+Task.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Task.belongsTo(Project, {
+  foreignKey: 'project_id',
+  onDelete: 'CASCADE'
+});
+
+module.exports = { User, Task, Project };
+ 
