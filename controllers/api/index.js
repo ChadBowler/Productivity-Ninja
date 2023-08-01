441899a1a538ae9544b15530d4db202e17db1c0b
Kyung Kwon <kyungkwon01@gmail.com>
2023-07-31 19:22:28 -0600

    added projectRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const taskRoutes = require('./taskRoutes');
+const projectRoutes = require('./projectRoutes');
 
 router.use('/users', userRoutes);
 router.use('/tasks', taskRoutes);
+router.use('/projectRoutes', projectRoutes);
 
 module.exports = router;
