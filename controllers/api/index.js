75c8ea8ff7366aadc5c4b7eb71a0a4fa29243876
Kyung Kwon <kyungkwon01@gmail.com>
2023-07-31 19:28:40 -0600

    fixed routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const taskRoutes = require('./taskRoutes');
-const projectRoutes = require('./projectRoutes');
+const userRoutes = require('./user-routes');
+const taskRoutes = require('./task-routes');
+const projectRoutes = require('./project-routes');
 
 router.use('/users', userRoutes);
 router.use('/tasks', taskRoutes);
