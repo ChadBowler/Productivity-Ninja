78399e283af2098943b50c3dcfd1740de6403745
Chad Bowler <chadbowler@gmail.com>
2023-08-02 17:53:21 -0600

    set up basic layout for projects page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { Project } = require('../models');
-const withAuth = require('../utils/auth');
+// const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   // res.render('homepage', { logged_in: req.session.logged_in });
