9118157b089303d9270dc06082d69842a3b33359
Maridon Romney <mkromney@gmail.com>
2023-08-02 21:53:07 -0600

    switched query ordering in database seeding.

--- seeds/seed.js
+++ seeds/seed.js
@@ -11,14 +11,14 @@ const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
 
+  await seedProjects();
+  console.log('\n----- PROJECTS SEEDED -----\n');
+
   await seedUsers();
   console.log('\n----- USERS SEEDED -----\n');
 
   await seedTasks();
   console.log('\n----- TASKS SEEDED -----\n');
-
-  await seedProjects();
-  console.log('\n----- PROJECTS SEEDED -----\n');
 
   process.exit(0);
 };
