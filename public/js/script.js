77b81956fc0a8adf09acff6f14bf526b5f4bff4f
Chad Bowler <chadbowler@gmail.com>
2023-08-03 13:55:10 -0600

    added functionality to add a user to a project in a project view

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,51 @@
-const openEdit = (e) => {
+const openEditForm = (e) => {
   e.preventDefault();
   let editForm = document.getElementById('edit-task-form');
   editForm.classList.remove('hidden');
 };
-const openNew = (e) => {
+const openTaskForm = (e) => {
   e.preventDefault();
 
   let newForm = document.getElementById('new-task-form');
   newForm.classList.remove('hidden');
+};
+const addEmployee = (e) => {
+  e.preventDefault();
+
+  let employeeSelectForm = document.getElementById('employee-select-form');
+  employeeSelectForm.classList.remove('hidden');
+};
+const addEmployeeToProject = async () => {
+  console.log('something');
+  const projectId = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+  const userName = document.querySelector('#employee-select').value;
+
+  if (userName) {
+    const response = await fetch('/api/users/add-to-project', {
+      method: 'PUT',
+      body: JSON.stringify({
+        project_id: projectId,
+        username: userName,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    //reload page after updating the task, or send alert if it didn't work
+    if (response.ok) {
+      document.location.reload();
+    } else {
+      console.log(response);
+      alert(response.statusText);
+    }
+  }
+};
+const selectEmployee = (e) => {
+  e.preventDefault();
+  addEmployeeToProject();
+  let employeeSelectForm = document.getElementById('employee-select-form');
+  employeeSelectForm.classList.add('hidden');
 };
 const closeBox = (e) => {
   e.preventDefault();
@@ -20,13 +58,18 @@ const closeBox = (e) => {
 
 let openEditButtons = document.querySelectorAll('.open-edit');
 openEditButtons.forEach(function(button) {
-  button.addEventListener('click', openEdit);
+  button.addEventListener('click', openEditForm);
 });
 let closeBoxButton = document.querySelectorAll('.btn-close');
 closeBoxButton.forEach(function(button) {
   button.addEventListener('click', closeBox);
 });
+let employeeSelect = document.getElementById('employee-select');
+employeeSelect.addEventListener('change', selectEmployee);
 
 document
   .querySelector('.new-task')
-  .addEventListener('click', openNew);
+  .addEventListener('click', openTaskForm);
+document
+  .querySelector('.new-employee')
+  .addEventListener('click', addEmployee);
