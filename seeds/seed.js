6e071038f7d0b75dc03ded39edabcaa3a1bab8ef
Maridon Romney <70175497+mkromney@users.noreply.github.com>
2023-08-02 21:54:17 -0600

    changed order of db query, moved projects to top.

--- seeds/seed.js
+++ seeds/seed.js
@@ -11,17 +11,18 @@ const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
 
+  await seedProjects();
+  console.log('\n----- PROJECTS SEEDED -----\n');
+
   await seedUsers();
   console.log('\n----- USERS SEEDED -----\n');
 
   await seedTasks();
   console.log('\n----- TASKS SEEDED -----\n');
 
-  await seedProjects();
-  console.log('\n----- PROJECTS SEEDED -----\n');
-
   process.exit(0);
 };
 
 // All databases are seeded. //
 seedAll();
+
