179592d9f910dfd66346ebbfde856067a403ef62
Maridon Romney <70175497+mkromney@users.noreply.github.com>
2023-08-02 21:58:23 -0600

    added project id and user id to tasks seed.

--- seeds/task-seeds.js
+++ seeds/task-seeds.js
@@ -2,31 +2,41 @@ const { Task } = require('../models');
 
 const taskData = [
   {
-    name: "Build API",
-    description: "Client has asked for new API, build and demo.",
-    status: false
+    name: 'Build API',
+    description: 'Client has asked for new API, build and demo.',
+    status: true,
+    user_id: 'saulbrego',
+    project_id: 1
   },
   {
-    name: "Overhaul Database",
-    description: "Database needs to be reconfigured and outdated entries removed.",
-    status: false
+    name: 'Overhaul Database',
+    description: 'Database needs to be reconfigured and outdated entries removed.',
+    status: false,
+    user_id: 'anikoasfaloth',
+    project_id: 2
   },
   {
-    name: "Develop Library",
-    description: "New library needed to improve company website.",
-    status: false
+    name: 'Develop Library',
+    description: 'New library needed to improve company website.',
+    status: false,
+    user_id: 'saulbrego',
+    project_id: 1
   },
   {
-    name: "Research Potential Frameworks",
-    description: "New frameworks needed for team to improve testing.",
-    status: false
+    name: 'Research Potential Frameworks',
+    description: 'New frameworks needed for team to improve testing.',
+    status: true,
+    user_id: 'saulbrego',
+    project_id: 2
   },
   {
-    name: "Fix Bugs",
-    description: "",
-    status: false
+    name: 'Fix Bugs',
+    description: '',
+    status: false,
+    user_id: 'anikoasfaloth',
+    project_id: 1
   }
-]
+];
 
 const seedTasks = () => Task.bulkCreate(taskData);
 
