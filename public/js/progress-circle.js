8a4789abd69f4ad4e8c4752792227b2f5d4c6324
Maridon Romney <mkromney@gmail.com>
2023-08-07 14:01:05 -0600

    added logic for complete project button in progress-circle.js

--- public/js/progress-circle.js
+++ public/js/progress-circle.js
@@ -1,14 +1,12 @@
-
 const completeTasks = function () {
   const ctasks = document.getElementById('complete-tasks').children[1].children;
   return ctasks.length;
 };
+
 const incompleteTasks = function () {
   const itasks = document.getElementById('incomplete-tasks').children[1].children;
   return itasks.length;
 };
-
-
 
 function fillProgressCircle(completeTasks, incompleteTasks) {
   let totalTasks = completeTasks + incompleteTasks;
@@ -20,9 +18,16 @@ function fillProgressCircle(completeTasks, incompleteTasks) {
 
   let progressCirclePercent = document.getElementById('progress-text');
   let progressCircle = document.getElementById('progress-circle');
+  let projectCompleteButton = document.getElementById('project-complete-button');
 
   progressCirclePercent.innerHTML = progressPercent.toFixed(0);
-  progressCircle.style=`background-image: conic-gradient(#08678C 0% ${progressPercent}%, black ${progressPercent}% 100%);`;
+  progressCircle.style = `background-image: conic-gradient(#08678C 0% ${progressPercent}%, black ${progressPercent}% 100%);`;
+
+  if (completeTasks > 0 && progressPercent === 100) {
+    projectCompleteButton.style.display = 'block';
+  } else {
+    projectCompleteButton.style.display = 'none';
+  }
 }
 
 fillProgressCircle(completeTasks(), incompleteTasks());
